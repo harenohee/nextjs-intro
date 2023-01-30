f743a5c2363fedf080243d7fcfc9634e18f82695
harenohee <panda4859@gmail.com>
2023-01-30 22:22:31 +0900

    css modules

---
+++ components/NavBar.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
+export default function NavBar() {
+  const router = useRouter();
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+    </nav>
+  );
+}
