be68050858dc3b369127a3379e16cf5368617730
harenohee <panda4859@gmail.com>
2023-01-31 19:21:46 +0900

    url 마스킹

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,16 @@ export default function Home({ results }) {
         >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>
-            <Link legacyBehavior href={`/movies/${movie.id}`}>
+            <Link
+              legacyBehavior
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+            >
               <a>{movie.original_title}</a>
             </Link>
           </h4>
