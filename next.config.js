6a7adf8fe40f40ddf461f19d5fba229f4bc766d7
harenohee <panda4859@gmail.com>
2023-01-31 14:54:01 +0900

    rewrite: redirect 시키지만 유저가 url변화를 보지 못한다.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = "d1a1c244c551297374694671a4b518e2";
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
@@ -7,6 +8,14 @@ const nextConfig = {
         source: "/old-blog/:path*",
         destination: "/new-blog/:path*",
         permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
       },
     ];
   },
