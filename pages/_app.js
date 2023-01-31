0e6dabed13c7653755c69b8637882c82e369b7c9
harenohee <panda4859@gmail.com>
2023-01-31 19:52:32 +0900

    style: global 스타일 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-import Layout from "components/Layout";
-
-function MyApp({ Component, pageProps }) {
+import Layout from "../components/Layout.js";
+import "../styles/globals.css";
+export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
 }
-export default MyApp;
