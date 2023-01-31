9332b30a7c195891d293d2c4a02a49cf47fa78d9
harenohee <panda4859@gmail.com>
2023-01-31 12:27:10 +0900

    fetch TMDB api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-import NavBar from "components/NavBar";
-
+// import NavBar from "components/NavBar";
+import Seo from "@/components/Seo";
+import { useState, useEffect } from "react";
+const API_KEY = "d1a1c244c551297374694671a4b518e2";
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    // 즉시실행 함수
+    (async () => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      );
+      const json = await res.json();
+    })();
+  }, []);
   return (
     <div>
-      <NavBar />
-      Home
+      <Seo title="Home" />
+      <h1 className="active">홈 컨텐츠</h1>
     </div>
   );
 }
